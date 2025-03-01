58bd36af16893b08a6da257e523d4c07aa305a4b
Jose Yanez <jlyp_sn@protonmail.com>
2025-02-28 18:24:01 -0600

    Add size setting button.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function rowMaker (divGrid) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
-    for (let i = 1; i <= 16; i++) {
+    for (i = 1; i <= size; i++) {
         divMaker(row);
     }
     divGrid.appendChild(row);
@@ -9,19 +9,38 @@ function rowMaker (divGrid) {
 
 function divMaker(divRow) {
     const div = document.createElement("div");
-    div.setAttribute("class", "square")
-    div.addEventListener("hover", () => {
-        div.setAttribute("style","background-color = 'black'");
+    div.setAttribute("class", "square");
+    div.style.padding = `${1000 / (4 * size)}px`
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
     });
     divRow.appendChild(div);
+}
+
+function makeNewGrid () {
+    size = +prompt("Set new square grid size (max. 100):");
+    container.textContent = "";
+    grid.textContent = "";
+    for (u = 1; u <= size; u++) {
+        rowMaker(grid);
+    }
+    container.appendChild(grid);
 }
 
 const container = document.querySelector(".container");
 const grid = document.createElement("div");
 grid.setAttribute("class", "grid");
 
-for (let u = 1; u <= 16; u++) {
+let u;
+let i;
+let size = 16;
+
+const setGridSize = document.querySelector(".set-grid-size");
+setGridSize.addEventListener("click", makeNewGrid);
+
+for (u = 1; u <= size; u++) {
     rowMaker(grid);
 }
 
 container.appendChild(grid);
+
